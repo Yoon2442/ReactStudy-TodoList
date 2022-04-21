1802b04a74e90e9c5989d0f8829dd7ccc7f5066c
Yoon2442 <yoonhw20@naver.com>
2022-04-21 17:44:59 +0900

    test

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -30,7 +30,7 @@ function TodoHead() {
     <TodoHeadBlock>
       <h1>2022년 4월 21일</h1>
       <div className="day">목요일</div>
-      <div className="tasks-left">할 일 2개 남 </div>
+      <div className="tasks-left">할 일 2개 남음 </div>
     </TodoHeadBlock>
   );
 }
